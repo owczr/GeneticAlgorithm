87583bd19426b03b2eef03abbf3c74240c9ede7e
Jakub Owczarek <jakub.owczarek@codete.com>
2023-03-20 10:29:49 +0100

    Add points distance function

--- main/main.py
+++ main/main.py
@@ -1,3 +1,5 @@
+import math
+
 import numpy as np
 
 import utils
@@ -7,9 +9,20 @@ def gen_points(x_lim, y_lim, count):
     x_list = np.random.randint(*x_lim, count)
     y_list = np.random.randint(*y_lim, count)
 
-    return x_list, y_list
+    return tuple(zip(x_list, y_list))
+
+
+def points_distance(point_1, point_2):
+    x_1 = point_1[0]
+    y_1 = point_1[1]
+    x_2 = point_2[0]
+    y_2 = point_2[1]
+
+    return math.sqrt((x_1 - x_2) ** 2 + (y_1 - y_2) ** 2)
 
 
 if __name__ == "__main__":
     utils.set_seeds()
-    print((gen_points(utils.X_LIM, utils.Y_LIM, utils.POINTS_NO)[0]))
+    points = gen_points(utils.X_LIM, utils.Y_LIM, utils.POINTS_NO)
+    # print(points_distance(points[0], points[1]))
+    print(points_distance((1, 1), (0, 0)))

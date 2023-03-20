20f9adce869220e4d5e4f239e0cfe6aef4c9cff4
Jakub Owczarek <jakub.owczarek@codete.com>
2023-03-20 10:32:57 +0100

    Add point package

--- main/main.py
+++ main/main.py
@@ -3,26 +3,10 @@ import math
 import numpy as np
 
 import utils
-
-
-def gen_points(x_lim, y_lim, count):
-    x_list = np.random.randint(*x_lim, count)
-    y_list = np.random.randint(*y_lim, count)
-
-    return tuple(zip(x_list, y_list))
-
-
-def points_distance(point_1, point_2):
-    x_1 = point_1[0]
-    y_1 = point_1[1]
-    x_2 = point_2[0]
-    y_2 = point_2[1]
-
-    return math.sqrt((x_1 - x_2) ** 2 + (y_1 - y_2) ** 2)
-
+from points import points
 
 if __name__ == "__main__":
     utils.set_seeds()
-    points = gen_points(utils.X_LIM, utils.Y_LIM, utils.POINTS_NO)
-    # print(points_distance(points[0], points[1]))
-    print(points_distance((1, 1), (0, 0)))
+    points_list = points.generate(utils.X_LIM, utils.Y_LIM, utils.POINTS_NO)
+    print(points.distance(points_list[0], points_list[1]))
+    print(points.distance((1, 1), (0, 0)))
---
+++ main/points/points.py
@@ -0,0 +1,20 @@
+import math
+
+import numpy as np
+
+
+def generate(x_lim, y_lim, count):
+    x_list = np.random.randint(*x_lim, count)
+    y_list = np.random.randint(*y_lim, count)
+
+    return tuple(zip(x_list, y_list))
+
+
+def distance(point_1, point_2):
+    x_1 = point_1[0]
+    y_1 = point_1[1]
+    x_2 = point_2[0]
+    y_2 = point_2[1]
+
+    return math.sqrt((x_1 - x_2) ** 2 + (y_1 - y_2) ** 2)
+

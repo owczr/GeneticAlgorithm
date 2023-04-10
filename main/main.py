533aab130e0131dad360f2fb9fa5ab53abe2c13f
Jakub Owczarek <jakub.owczarek@codete.com>
2023-04-10 14:20:46 +0200

    Move logic to algorithm.py

--- main/main.py
+++ main/main.py
@@ -1,35 +1,8 @@
-from src import points, utils
-from src.population import Population
+from src import algorithm
 
 
 def main():
-    utils.set_seeds()
-    points_list = points.generate(utils.X_LIM, utils.Y_LIM, utils.POINTS_NO)
-
-    # Generate initial population
-    initial_population = Population.generate_first(10, utils.POINTS_NO)
-
-    # Create Population object
-    population = Population(initial_population, utils.POINTS_NO, points_list)
-
-    # Calculate distances
-    population.calculate_distances()
-
-    # Select best elements
-    population.select_elite("roulette")
-
-    # Create children elements
-    children_permutations = population.crossover(keep_parents=True)
-
-    # Create new Population object
-    children_elements = Population.generate_elements(children_permutations)
-    population = Population(children_elements, utils.POINTS_NO, points_list)
-
-    # Mutate elements
-    population.mutate()
-
-    print(len(population.elements))
-
+    algorithm.run()
     # for prob, dist, pop in zip(probabilities, distance_list, population):
     #     print(prob, sum(dist), pop)
 
